0e91e9c9a417e7cee68e3b59d0945c507004a151
nguyencuong <thecuongthehieu@gmail.com>
2019-03-17 12:04:52 +0700

    Scheme Language / Haven't Completed

---
+++ Assignment /Project 4_Assignment 8,9,10,11/Assignment 8/hw08/hw08.scm
@@ -0,0 +1,94 @@
+(define (cddr s)
+  (cdr (cdr s)))
+
+(define (cadr s)
+  'YOUR-CODE-HERE
+  (car (cdr s))
+)
+
+(define (caddr s)
+  'YOUR-CODE-HERE
+  (car (cddr s)) 
+)
+
+(define (sign x)
+  'YOUR-CODE-HERE
+  (cond ((< x 0) -1) ((> x 0) 1) (else 0))
+)
+
+(define (square x) (* x x))
+
+(define (pow b n)
+  'YOUR-CODE-HERE
+  (cond
+    ((= n 0) 1)
+    ((even? n) (square (pow b (/ n 2))))
+    (else (* b (square (pow b (quotient n 2)))))
+  )
+)
+
+(define (ordered? s)
+  'YOUR-CODE-HERE
+)
+
+(define (nodots s)
+  'YOUR-CODE-HERE
+)
+
+; Sets as sorted lists
+
+(define (empty? s) (null? s))
+
+(define (contains? s v)
+    (cond ((empty? s) #f)
+          'YOUR-CODE-HERE
+          (else nil) ; replace this line
+          ))
+
+; Equivalent Python code, for your reference:
+;
+; def empty(s):
+;     return s is Link.empty
+;
+; def contains(s, v):
+;     if empty(s):
+;         return False
+;     elif s.first > v:
+;         return False
+;     elif s.first == v:
+;         return True
+;     else:
+;         return contains(s.rest, v)
+
+(define (add s v)
+    (cond ((empty? s) (list v))
+          'YOUR-CODE-HERE
+          (else nil) ; replace this line
+          ))
+
+(define (intersect s t)
+    (cond ((or (empty? s) (empty? t)) nil)
+          'YOUR-CODE-HERE
+          (else nil) ; replace this line
+          ))
+
+; Equivalent Python code, for your reference:
+;
+; def intersect(set1, set2):
+;     if empty(set1) or empty(set2):
+;         return Link.empty
+;     else:
+;         e1, e2 = set1.first, set2.first
+;         if e1 == e2:
+;             return Link(e1, intersect(set1.rest, set2.rest))
+;         elif e1 < e2:
+;             return intersect(set1.rest, set2)
+;         elif e2 < e1:
+;             return intersect(set1, set2.rest)
+
+(define (union s t)
+    (cond ((empty? s) t)
+          ((empty? t) s)
+          'YOUR-CODE-HERE
+          (else nil) ; replace this line
+          ))
